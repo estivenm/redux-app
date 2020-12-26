52ad5c98e7025381d4dcfc7ef4e6c8bb8a616eeb
jemazo <jemazo@bancolombia.com.co>
2020-12-26 14:53:59 -0500

    agrega fuentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// ngrx
+import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
+import { HijoComponent } from './contador/hijo/hijo.component';
+import { NietoComponent } from './contador/nieto/nieto.component';
+import { contadorReducer } from './contador/contador.reducer';
+
+@NgModule({
+  declarations: [AppComponent, HijoComponent, NietoComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ contador: contadorReducer })],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
